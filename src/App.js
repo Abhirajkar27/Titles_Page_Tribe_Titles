afbe3c53613b6fc8788f35b5c50438bca51dbe31
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-08-24 14:53:07 +0530

    tribe

--- src/App.js
+++ src/App.js
@@ -1,31 +1,115 @@
-import React, { useState } from 'react'
-import EarnTitlePage from './Pages/EarnTitlePage'
-import AchievementPage from './Pages/AchievementPage'
-import VoteReveal from './Pages/VoteReveal';
+import React, { useState } from "react";
+import EarnTitlePage from "./Pages/EarnTitlePage";
+import AchievementPage from "./Pages/AchievementPage";
+import VoteReveal from "./Pages/VoteReveal";
+import './App.css';
 
 const App = () => {
   const [gameSTIndex, setGameSTIndex] = useState(1);
 
-
   let content;
   switch (gameSTIndex) {
     case 0:
-      content = <AchievementPage setGameSTIndex={setGameSTIndex}/>;
+      content = <AchievementPage setGameSTIndex={setGameSTIndex} />;
       break;
     case 1:
-      content = <VoteReveal setGameSTIndex={setGameSTIndex}/>;
+      content = <VoteReveal setGameSTIndex={setGameSTIndex} />;
       break;
     default:
-      content = <EarnTitlePage setGameSTIndex={setGameSTIndex}/>;
+      content = <EarnTitlePage setGameSTIndex={setGameSTIndex} />;
   }
-
 
   return (
     <>
+    <div className="bkg_SVG_Container_TPTA">
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 340 450"
+        fill="none"
+      >
+        <path
+          d="M171.106 443.065L-221.128 1.68755L-270.271 50.8306L171.106 443.065Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 350 501"
+        fill="none"
+      >
+        <path
+          d="M175.099 493.5L-89.5338 -34.3555L-149.721 0.393874L175.099 493.5Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 72 496"
+        fill="none"
+      >
+        <path
+          d="M35.7965 486.953L1.04707 -102.5H70.5459L35.7965 486.953Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 360 500"
+        fill="none"
+      >
+        <path
+          d="M178.85 491.662L59.8536 -86.6998L-7.2771 -68.7122L178.85 491.662Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 10 500"
+        fill="none"
+      >
+        <path
+          d="M2.7931 491.662L121.789 -86.6998L188.92 -68.7122L2.7931 491.662Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 10 501"
+        fill="none"
+      >
+        <path
+          d="M4.5443 493.5L269.177 -34.3555L329.365 0.393874L4.5443 493.5Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      <svg
+        className="bkg_SVG_TPTA"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 15 450"
+        fill="none"
+      >
+        <path
+          d="M6.5369 443.065L398.771 1.68755L447.914 50.8306L6.5369 443.065Z"
+          stroke="white"
+          stroke-opacity="0.1"
+        />
+      </svg>
+      </div>
       {content}
     </>
   );
+};
 
-}
-
-export default App
+export default App;
